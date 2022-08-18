3a6d1db86b219702a7a8719c4db8fea46ff72674
MiguelBits <mcv.cooper@gmail.com>
2022-08-18 17:15:44 +0100

    tokenName tokenSymbol removal, safeTransferRemoved

---
+++ test/AssertTest.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.15;
+
+import "forge-std/Test.sol";
+import {Vault} from "../src/Vault.sol";
+import {VaultFactory} from "../src/VaultFactory.sol";
+import {Controller} from "../src/Controller.sol";
+import {ERC20} from "@solmate/tokens/ERC20.sol";
+
+import {IWETH} from "./interfaces/IWETH.sol";
+
+contract SemiFungibleVaultTest is Test {
+    
+    function setUp() public {
+
+    }
+
+    function testMarketCreation(address ) public {
+
+    }
+
+}
