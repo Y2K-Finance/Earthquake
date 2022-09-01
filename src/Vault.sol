16fec1e92236d8178c1343dd0959ddec9e031c45
0xnexusflip <0xnexusflip@gmail.com>
2022-09-01 16:54:37 +0100

    add full NatSpec (main sync)

--- src/Vault.sol
+++ src/Vault.sol
@@ -58,30 +58,40 @@ contract Vault is SemiFungibleVault, ReentrancyGuard {
                                 MODIFIERS
     //////////////////////////////////////////////////////////////*/
 
+    /** @notice Only factory addresses can call functions that use this modifier
+      */
     modifier onlyFactory() {
         if(msg.sender != factory)
             revert AddressNotFactory(msg.sender);
         _;
     }
 
+    /** @notice Only controller addresses can call functions that use this modifier
+      */
     modifier onlyController() {
         if(msg.sender != controller)
             revert AddressNotController(msg.sender);
         _;
     }
 
+    /** @notice Only market addresses can call functions that use this modifier
+      */
     modifier marketExists(uint256 id) {
         if(idExists[id] != true)
             revert MarketEpochDoesNotExist(id);
         _;
     }
 
+    /** @notice You can only call functions that use this modifier before the current epoch has started
+      */
     modifier epochHasNotStarted(uint256 id) {
         if(block.timestamp > idEpochBegin[id] - timewindow)
             revert EpochAlreadyStarted(id);
         _;
     }
 
+    /** @notice You can only call functions that use this modifier after the current epoch has started
+      */
     modifier epochHasEnded(uint256 id) {
         if((block.timestamp < id) && idDepegged[id] == false)
             revert EpochNotFinished(id);
@@ -163,6 +173,12 @@ contract Vault is SemiFungibleVault, ReentrancyGuard {
         return shares;
     }
 
+    /**
+        @notice Deposit ETH function
+        @param  id  uint256 in UNIX timestamp, representing the end date of the epoch. Example: Epoch ends in 30th June 2022 at 00h 00min 00sec: 1654038000;
+        @param receiver  address of the receiver of the shares provided by this function, that represent the ownership of the deposited asset;
+        @return shares how many assets the owner is entitled to, removing the fee from its shares;
+     */
     function depositETH(uint256 id, address receiver)
         external
         payable
@@ -180,9 +196,9 @@ contract Vault is SemiFungibleVault, ReentrancyGuard {
     @notice Withdraw entitled deposited assets, checking if a depeg event //TODO add GOV token rewards
     @param  id uint256 in UNIX timestamp, representing the end date of the epoch. Example: Epoch ends in 30th June 2022 at 00h 00min 00sec: 1654038000;
     @param assets   uint256 of how many assets you want to withdraw, this value will be used to calculate how many assets you are entitle to according to the events;
-    @param receiver  address of the receiver of the assets provided by this function, that represent the ownership of the transfered asset;
-    @param owner    address of the owner of these said assets;
-    @return shares how many shares the owner is entitled to, according to the conditions;
+    @param receiver  Address of the receiver of the assets provided by this function, that represent the ownership of the transfered asset;
+    @param owner    Address of the owner of these said assets;
+    @return shares How many shares the owner is entitled to, according to the conditions;
      */
     function withdraw(
         uint256 id,
@@ -236,7 +252,10 @@ contract Vault is SemiFungibleVault, ReentrancyGuard {
     }
 
     /**
-    @notice calculates how much ether the %fee is taking from @param amount
+    @notice Calculates how much ether the %fee is taking from @param amount
+    @param amount Amount to withdraw from vault
+    @param _epoch Target epoch
+    @return feeValue Current fee value
      */
     function calculateWithdrawalFeeValue(uint256 amount, uint256 _epoch)
         public
@@ -251,16 +270,28 @@ contract Vault is SemiFungibleVault, ReentrancyGuard {
                            Factory FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
+    /**
+    @notice Factory function, changes treasury address
+    @param _treasury New treasury address
+     */
     function changeTreasury(address _treasury) public onlyFactory {
         if(_treasury == address(0))
             revert AddressZero();
         treasury = _treasury;
     }
 
+    /**
+    @notice Factory function, changes vault time window
+    @param _timewindow New vault time window
+     */
     function changeTimewindow(uint256 _timewindow) public onlyFactory {
         timewindow = _timewindow;
     }
 
+    /**
+    @notice Factory function, changes controller address
+    @param _controller New controller address
+     */
     function changeController(address _controller) public onlyFactory {
         if(_controller == address(0))
             revert AddressZero();
@@ -268,10 +299,10 @@ contract Vault is SemiFungibleVault, ReentrancyGuard {
     }
 
     /**
-    @notice function to deploy hedge assets for given epochs, after the creation of this vault.
-    @param  epochBegin uint256 in UNIX timestamp, representing the begin date of the epoch. Example: Epoch begins in 31/May/2022 at 00h 00min 00sec: 1654038000;
-    @param  epochEnd uint256 in UNIX timestamp, representing the end date of the epoch and also the ID for the minting functions. Example: Epoch ends in 30th June 2022 at 00h 00min 00sec: 1656630000;
-    @param _withdrawalFee uint256 of the fee value, multiply your % value by 10, Example: if you want fee of 0.5% , insert 5;
+    @notice Function to deploy hedge assets for given epochs, after the creation of this vault
+    @param  epochBegin uint256 in UNIX timestamp, representing the begin date of the epoch. Example: Epoch begins in 31/May/2022 at 00h 00min 00sec: 1654038000
+    @param  epochEnd uint256 in UNIX timestamp, representing the end date of the epoch and also the ID for the minting functions. Example: Epoch ends in 30th June 2022 at 00h 00min 00sec: 1656630000
+    @param _withdrawalFee uint256 of the fee value, multiply your % value by 10, Example: if you want fee of 0.5% , insert 5
      */
     function createAssets(uint256 epochBegin, uint256 epochEnd, uint256 _withdrawalFee)
         public
@@ -299,8 +330,8 @@ contract Vault is SemiFungibleVault, ReentrancyGuard {
 
     /**
     @notice Controller can call this function to trigger the end of the epoch, storing the TVL of that epoch and if a depeg event occurred
-    @param  id uint256 in UNIX timestamp, representing the end date of the epoch. Example: Epoch ends in 30th June 2022 at 00h 00min 00sec: 1654038000;
-    @param depeg boolean value indicating if the depeg event occurred, or not. Example: If depeg occurred depeg = true;
+    @param  id uint256 in UNIX timestamp, representing the end date of the epoch. Example: Epoch ends in 30th June 2022 at 00h 00min 00sec: 1654038000
+    @param depeg Boolean value indicating if the depeg event occurred, or not. Example: If depeg occurred depeg = true
      */
     function endEpoch(uint256 id, bool depeg)
         public
@@ -312,9 +343,9 @@ contract Vault is SemiFungibleVault, ReentrancyGuard {
     }
 
     /**
-    @notice Function to be called after endEpoch, by the Controller only, this function stores the TVL of the counterparty vault in a mapping to be used for later calculations of the entitled withdraw.
-    @param  id uint256 in UNIX timestamp, representing the end date of the epoch. Example: Epoch ends in 30th June 2022 at 00h 00min 00sec: 1654038000;
-    @param claimTVL uint256 representing the TVL the counterparty vault has, storing this value in a mapping;
+    @notice Function to be called after endEpoch, by the Controller only, this function stores the TVL of the counterparty vault in a mapping to be used for later calculations of the entitled withdraw
+    @param  id uint256 in UNIX timestamp, representing the end date of the epoch. Example: Epoch ends in 30th June 2022 at 00h 00min 00sec: 1654038000
+    @param claimTVL uint256 representing the TVL the counterparty vault has, storing this value in a mapping
      */
     function setClaimTVL(uint256 id, uint256 claimTVL) public onlyController {
         idClaimTVL[id] = claimTVL;
@@ -322,9 +353,9 @@ contract Vault is SemiFungibleVault, ReentrancyGuard {
 
     /**
     solhint-disable-next-line max-line-length
-    @notice Function to be called after endEpoch and setClaimTVL functions, respecting the calls in order, after storing the TVL of the end of epoch and the TVL amount to claim, this function will allow the transfer of tokens to the counterparty vault;
-    @param  id uint256 in UNIX timestamp, representing the end date of the epoch. Example: Epoch ends in 30th June 2022 at 00h 00min 00sec: 1654038000;
-    @param _counterparty address of the other vault, meaning address of the risk vault, if this is an hedge vault, and vice-versa;
+    @notice Function to be called after endEpoch and setClaimTVL functions, respecting the calls in order, after storing the TVL of the end of epoch and the TVL amount to claim, this function will allow the transfer of tokens to the counterparty vault
+    @param  id uint256 in UNIX timestamp, representing the end date of the epoch. Example: Epoch ends in 30th June 2022 at 00h 00min 00sec: 1654038000
+    @param _counterparty Address of the other vault, meaning address of the risk vault, if this is an hedge vault, and vice-versa
     */
     function sendTokens(uint256 id, address _counterparty)
         public
@@ -340,9 +371,9 @@ contract Vault is SemiFungibleVault, ReentrancyGuard {
 
     /**
     @notice Calculations of how much the user will receive;
-    @param  id uint256 in UNIX timestamp, representing the end date of the epoch. Example: Epoch ends in 30th June 2022 at 00h 00min 00sec: 1654038000;
-    @param amount uint256 of the amount the user wants to withdraw;
-    @return entitledAmount how much amount the user will receive, according to the conditions;
+    @param  id uint256 in UNIX timestamp, representing the end date of the epoch. Example: Epoch ends in 30th June 2022 at 00h 00min 00sec: 1654038000
+    @param amount uint256 of the amount the user wants to withdraw
+    @return entitledAmount How much amount the user will receive, according to the conditions
     */
     function beforeWithdraw(uint256 id, uint256 amount)
         public
@@ -393,11 +424,17 @@ contract Vault is SemiFungibleVault, ReentrancyGuard {
 
         return entitledAmount;
     }
-
+    
+    /** @notice Lookup total epochs length
+      */
     function epochsLength() public view returns (uint256) {
         return epochs.length;
     }
 
+    /** @notice Lookup next epochs' end from target
+        @param _epoch Target epoch
+        @return nextEpochEnd Next epoch end
+      */
     function getNextEpoch(uint256 _epoch)
         public
         view
