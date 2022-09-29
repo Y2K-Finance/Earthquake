926604605dc94db5bcfee98fcc2f257c4367adb5
MiguelBits <mcv.cooper@gmail.com>
2022-09-29 18:19:02 +0100

    removed admin

--- script/DeployGoerli.s.sol
+++ script/DeployGoerli.s.sol
@@ -54,7 +54,7 @@ contract DeployGoerliScript is Script {
 
         // setUp();
         vaultFactory = new VaultFactory(admin,WETH,admin);
-        controller = new Controller(address(vaultFactory),admin, arbitrum_sequencer);
+        controller = new Controller(address(vaultFactory), arbitrum_sequencer);
 
         vaultFactory.setController(address(controller));
 
