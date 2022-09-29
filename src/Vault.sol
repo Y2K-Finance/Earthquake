a572d35dff3f8d01eef9a41fd7e5792a13baf376
MiguelBits <mcv.cooper@gmail.com>
2022-09-29 02:57:30 +0100

    assets & msg.value

--- src/Vault.sol
+++ src/Vault.sol
@@ -184,7 +184,7 @@ contract Vault is SemiFungibleVault, ReentrancyGuard {
         IWETH(address(asset)).deposit{value: msg.value}();
         _mint(receiver, id, msg.value, EMPTY);
 
-        emit Deposit(msg.sender, receiver, id, assets, assets);
+        emit Deposit(msg.sender, receiver, id, msg.value, msg.value);
     }
 
     /**
