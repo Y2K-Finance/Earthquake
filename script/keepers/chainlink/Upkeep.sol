da2380e05d0f36642a7818a7bdce54e66f56c277
MiguelBits <mcruzvas@gmail.com>
2022-11-18 10:43:24 +0000

    next steps?

--- script/keepers/chainlink/Upkeep.sol
+++ script/keepers/chainlink/Upkeep.sol
@@ -4,51 +4,90 @@ pragma solidity ^0.8.7;
 // AutomationCompatible.sol imports the functions from both ./AutomationBase.sol and
 // ./interfaces/AutomationCompatibleInterface.sol
 import "@chainlink/AutomationCompatible.sol";
+import "./interfaces/IUpkeepRefunder.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IController} from "../../../src/interfaces/IController.sol";
 
+interface KeeperRegistryLike {
+    function getUpkeep(uint256 id)
+        external
+        view
+        returns (
+            address target,
+            uint32 executeGas,
+            bytes memory checkData,
+            uint96 balance,
+            address lastKeeper,
+            address admin,
+            uint64 maxValidBlocknumber
+        );
 
-contract UpkeepController is AutomationCompatibleInterface {
+    function addFunds(uint256 id, uint96 amount) external;
+}
+
+contract UpkeepController is Ownable, AutomationCompatibleInterface {
+
+    KeeperRegistryLike public keeperRegistry;
     IController controller;
     address public owner;
-    uint256 public marketIndex;
-    uint256 public epochBegin;
-    uint256 public epochId;
+    address public linkToken;
+    // uint256 public marketIndex;
+    // uint256 public epochBegin;
+    // uint256 public epochId;
+    uint256 public biggestMarketIndex;
 
-    modifier onlyOwner{
-        require(msg.sender == owner, "Only owner can call this function");
-        _;
-    }
-    constructor(IController _controller, uint _marketIndex, uint _epochBegin, uint _epochId){
+    mapping(uint256 => uint256) public marketIndexToCurrentEpochId;
+
+    constructor(IController _controller, address _linkToken/*, uint _marketIndex, uint _epochBegin, uint _epochId*/){
         controller = IController(_controller);
-        owner = msg.sender;
-        marketIndex = _marketIndex;
-        epochBegin = _epochBegin;
-        epochId = _epochId;
+        // marketIndex = _marketIndex;
+        // epochBegin = _epochBegin;
+        // epochId = _epochId;
+        linkToken = _linkToken;
+    }
+
+
+    function startTask(uint256 _marketIndex, uint256 _epochId) public onlyOwner {
+        if(biggestMarketIndex < _marketIndex) {
+            biggestMarketIndex = _marketIndex;
+        }
+
+        marketIndexToCurrentEpochId[_marketIndex] = _epochId;
     }
 
     function checkUpkeep(bytes calldata /* checkData */) external view override returns (bool upkeepNeeded, bytes memory /* performData */) {
-            if(block.timestamp < epochBegin){
-                return (false, "");
-            }
 
-            else if(block.timestamp < epochId){
-                return (true, 
-                abi.encodeWithSelector(
-                    bytes4(keccak256("triggerDepeg(uint256,uint256)")), 
-            marketIndex, epochID));
-            }
+        // for (uint i = 0; i < biggestMarketIndex; i++) {
 
-            else{
-                return (true, abi.encodeWithSelector(
-                    bytes4(keccak256("triggerEndEpoch(uint256,uint256)")), 
-            marketIndex, epochID));
-            }
+        //     if(block.timestamp < epochBegin){
+        //         return (false, "");
+        //     }
+
+        //     else if(block.timestamp < epochId){
+        //         return (true, 
+        //         abi.encodeWithSelector(
+        //             bytes4(keccak256("triggerDepeg(uint256,uint256)")), 
+        //     marketIndex, epochID));
+        //     }
+
+        //     else{
+        //         return (true, abi.encodeWithSelector(
+        //             bytes4(keccak256("triggerEndEpoch(uint256,uint256)")), 
+        //     marketIndex, epochID));
+        //     }
+        // }
+
+        return true;
     }
 
     function performUpkeep(bytes calldata performData) external override {
-        (bool success, ) = controller.call(performData);
-        require(success, "performUpkeep: call failed");
-        //withdraw funds
+        for (uint i = 0; i < biggestMarketIndex; i++) {
+            performData = abi.encodeWithSelector(bytes4(keccak256("triggerDepeg(uint256,uint256)")), i, marketIndexToCurrentEpochId[i]);
+            (bool success, ) = controller.call(performData);
+
+            // require(success, "performUpkeep: call failed");
+            //withdraw funds?
+        }
     }
 }
 
