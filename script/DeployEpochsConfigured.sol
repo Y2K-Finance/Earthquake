207fceca6e8dbe318cadf93a40cf0db6d846386d
MiguelBits <mcv.cooper@gmail.com>
2022-11-29 19:59:16 +0000

    epochs

--- script/DeployEpochsConfigured.sol
+++ script/DeployEpochsConfigured.sol
@@ -14,36 +14,30 @@ contract ConfigEpochsScript is Script, HelperConfig {
         vm.startBroadcast();
 
         ConfigAddresses memory addresses = getConfigAddresses();
-        ConfigMarket memory markets = getConfigEpochs(index);
+        ConfigEpochs memory epochs = getConfigEpochs(index);
         ConfigFarm memory farms = getConfigFarm(index);
 
         contractToAddresses(addresses);
 
         //INDEX
-        //get markets config
-        console2.log("Market name", markets.name);
-        console2.log("Adress token", addresses.tokenFRAX);
-        console2.log("Market token", markets.token);
-        console2.log("Adress oracle", addresses.oracleFRAX);
-        console2.log("Market oracle", markets.oracle);
-        console2.log("Market strike price", uint256(markets.strikePrice));
-        console2.log("Market epoch begin", markets.epochBegin);
-        console2.log("Market epoch   end", markets.epochEnd);
-        console2.log("Market epoch fee", markets.epochFee);
+        //get epochs config
+        console2.log("Epoch epoch begin", epochs.epochBegin);
+        console2.log("Epoch epoch   end", epochs.epochEnd);
+        console2.log("Epoch epoch fee", epochs.epochFee);
         console2.log("Farm rewards amount HEDGE", farms.rewardsAmountHEDGE);
         console2.log("Farm rewards amount RISK", farms.rewardsAmountRISK);
         //console2.log("Sender balance amnt", y2k.balanceOf(msg.sender));
         console2.log("\n");
-        // create market 
-        vaultFactory.deployMoreAssets(index, markets.epochBegin, markets.epochEnd, markets.epochFee);
-        (address rHedge, address rRisk) = rewardsFactory.createStakingRewards(index, markets.epochEnd);
+        // create Epoch 
+        vaultFactory.deployMoreAssets(index, epochs.epochBegin, epochs.epochEnd, epochs.epochFee);
+        (address rHedge, address rRisk) = rewardsFactory.createStakingRewards(index, epochs.epochEnd);
         //sending gov tokens to farms
         y2k.transfer(rHedge, stringToUint(farms.rewardsAmountHEDGE));
         y2k.transfer(rRisk, stringToUint(farms.rewardsAmountRISK));
         //start rewards for farms
         StakingRewards(rHedge).notifyRewardAmount(y2k.balanceOf(rHedge));
         StakingRewards(rRisk).notifyRewardAmount(y2k.balanceOf(rRisk));
-        // stop create market
+        // stop create Epoch
         
         //transfer onwership of farms
         // StakingRewards(rHedge).setRewardsDistribution(addresses.policy);
@@ -54,7 +48,7 @@ contract ConfigEpochsScript is Script, HelperConfig {
         console2.log("Farm Hedge", rHedge);
         console2.log("Farm Risk", rRisk);
 
-        startKeepers(index, markets.epochEnd);
+        startKeepers(index, epochs.epochEnd);
 
         console.log("ConfigEpochsScript done");
 
