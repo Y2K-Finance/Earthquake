31bf400a847d55601e12aa3beeea18247181b757
MiguelBits <mcruzvas@gmail.com>
2022-12-08 02:05:29 +0000

    new script config

--- script/Helper.sol
+++ script/Helper.sol
@@ -17,6 +17,15 @@ import "./keepers/KeeperEndEpoch.sol";
 contract HelperConfig is Script {
     using stdJson for string;
 
+    struct ConfigVariables{
+        uint256 amountOfNewEpochs;
+        uint256 amountOfNewMarkets;
+        uint256[] epochsIds;
+        uint256[] marketsIds;
+        bool newEpochs;
+        bool newFarms;
+        bool newMarkets;
+    }
     struct ConfigAddresses {
         address admin;
         address arbitrum_sequencer;
@@ -46,36 +55,25 @@ contract HelperConfig is Script {
     }
 
     struct ConfigMarket {
-        uint256 epochBegin;
-        uint256 epochEnd;
-        uint256 epochFee;
-        string farmRewardsHEDGE;
-        string farmRewardsRISK;
         uint256 index;
         string name;
         address oracle;
         int256 strikePrice;
         address token;
     }
-
-        struct ConfigMarket1 {
-        uint256 index;
-        string name;
-        address oracle;
-        int256 strikePrice;
-        address token;
-        }
 
     struct ConfigEpochs {
         uint256 epochBegin;
         uint256 epochEnd;
         uint256 epochFee;
-        string farmRewardsHEDGE;
-        string farmRewardsRISK;
         uint256 index;
     }
 
-    bool newMarkets;
+    struct ConfigFarms {
+        string farmRewardsHEDGE;
+        string farmRewardsRISK;
+        uint index;
+    }
 
     VaultFactory vaultFactory;
     Controller controller;
@@ -83,7 +81,57 @@ contract HelperConfig is Script {
     Y2K y2k;
     KeeperGelatoDepeg keeperDepeg;
     KeeperGelatoEndEpoch keeperEndEpoch;
+    ConfigVariables configVariables;
+    function setVariables() public {
 
+        uint256[] memory _marketsIds;
+        uint256 _amountOfNewMarkets;
+
+        uint256 _amountOfNewEpochs;
+        uint256[] memory _epochsIds;
+
+        string memory root = vm.projectRoot();
+        string memory path = string.concat(root, "/configJSON.json");
+        string memory json = vm.readFile(path);
+        bytes memory parseJsonByteCode = json.parseRaw(".newMarkets");
+        bool _newMarkets = abi.decode(parseJsonByteCode, (bool));
+        if(_newMarkets) {
+            parseJsonByteCode = json.parseRaw(".amountOfNewMarkets");
+            _amountOfNewMarkets = abi.decode(parseJsonByteCode, (uint256));
+            parseJsonByteCode = json.parseRaw(".marketsIds");
+            _marketsIds = abi.decode(parseJsonByteCode, (uint256[]));
+            console2.log("newMarkets", _newMarkets);
+            console2.log("amountOfNewMarkets", _amountOfNewMarkets);
+            for(uint i = 0; i < _amountOfNewMarkets; i++) {
+                console2.log("marketsIds", _marketsIds[i]);
+            }
+        }
+
+        parseJsonByteCode = json.parseRaw(".newEpochs");
+        bool _newEpochs = abi.decode(parseJsonByteCode, (bool));
+        if(_newEpochs) {
+            parseJsonByteCode = json.parseRaw(".amountOfNewEpochs");
+            _amountOfNewEpochs = abi.decode(parseJsonByteCode, (uint256));
+            parseJsonByteCode = json.parseRaw(".epochsIds");
+            _epochsIds = abi.decode(parseJsonByteCode, (uint256[]));
+            console2.log("newEpochs", _newEpochs);
+            console2.log("amountOfNewEpochs", _amountOfNewEpochs);
+            for(uint i = 0; i < _amountOfNewEpochs; i++) {
+                console2.log("epochsIds", _epochsIds[i]);
+            }
+        }
+        bool _newFarms = abi.decode(json.parseRaw(".newFarms"), (bool));
+
+        configVariables = ConfigVariables({
+            amountOfNewEpochs: _amountOfNewEpochs,
+            amountOfNewMarkets: _amountOfNewMarkets,
+            epochsIds: _epochsIds,
+            marketsIds: _marketsIds,
+            newEpochs: _newEpochs,
+            newFarms: _newFarms,
+            newMarkets: _newMarkets
+        });
+    }
     function contractToAddresses(ConfigAddresses memory configAddresses) public {
         vaultFactory = VaultFactory(configAddresses.vaultFactory);
         controller = Controller(configAddresses.controller);
@@ -107,80 +155,52 @@ contract HelperConfig is Script {
         //console2.log("ConfigAddresses ", rawConstants.weth);
         return rawConstants;
     }
-
-      function getConfigMarket() public returns (bool newMarkets, uint256 amount,uint256[] memory  marketIds) {
-        string memory root = vm.projectRoot();
-        string memory path = string.concat(root, "/configJSON.json");
-        string memory json = vm.readFile(path);
-        bytes memory parseJsonByteCode = json.parseRaw(".newMarkets");
-        newMarkets = abi.decode(parseJsonByteCode, (bool));
-        if(newMarkets) {
-                bytes memory parseJsonByteCode2 = json.parseRaw(".amountOfNewMarkets");
-                amount = abi.decode(parseJsonByteCode2, (uint256));
-                bytes memory parseJsonByteCode3 = json.parseRaw(".marketIds");
-                marketIds = abi.decode(parseJsonByteCode3, (uint256[amount]));
-        }
-
-        //console2.log("ConfigAddresses ", rawConstants.weth);
-        // return rawConstants;
-    }
-
-
-      function getMarketfromJson(uint256 index, uint256 marketId) public returns (ConfigMarket1 memory marketStruct) {
-        string memory root = vm.projectRoot();
-        string memory path = string.concat(root, "/configJSON.json");
-        string memory json = vm.readFile(path);
-        
-        string memory pathString = string.concat(
-          ".markets",
-         "[", Strings.toString(index), "]")
-        bytes memory parseJsonByteCode = json.parseRaw(pathString);
-        marketStruct = abi.decode(parseJsonByteCode, (ConfigMarket1));
-        console2.log("ConfigMarkets ",ConfigMarket1.name);
-        //console2.log("ConfigAddresses ", rawConstants.weth);
-        // return rawConstants;
-    }
-    
-
     function getConfigMarket(uint256 index) public returns (ConfigMarket memory) {
-        index = index - 1;
         string memory root = vm.projectRoot();
-        string memory path = string.concat(root, "/configY2K.json");
+        string memory path = string.concat(root, "/configJSON.json");
         string memory json = vm.readFile(path);
-        string memory indexString = string.concat(".",
-          "markets");
-        bytes memory parseJsonByteCode = json.parseRaw(".markets");
-        ConfigMarket[] memory rawConstants = abi.decode(parseJsonByteCode, (ConfigMarket[]));
-        console2.log("ConfigMarkets ", rawConstants[0].name);
-        return rawConstants[0];
+        string memory indexString = string.concat(".markets", "[", Strings.toString(index), "]");
+        bytes memory transactionDetails = json.parseRaw(indexString);
+        ConfigMarket memory rawConstants = abi.decode(transactionDetails, (ConfigMarket));
+        //console2.log("ConfigMarkets ", rawConstants.name);
+        return rawConstants;
     }
 
     function getConfigEpochs(uint256 index) public returns (ConfigEpochs memory) {
         string memory root = vm.projectRoot();
-        string memory path = string.concat(root, "/configY2K.json");
+        string memory path = string.concat(root, "/configJSON.json");
         string memory json = vm.readFile(path);
-        string memory indexString = string.concat(".",
-          "epochs",
-         "[", Strings.toString(index - 1), "]");
-        bytes memory parseJsonByteCode = json.parseRaw(indexString);
-        ConfigEpochs memory rawConstants = abi.decode(parseJsonByteCode, (ConfigEpochs));
+        string memory indexString = string.concat(".epochs", "[", Strings.toString(index), "]");
+        bytes memory transactionDetails = json.parseRaw(indexString);
+        ConfigEpochs memory rawConstants = abi.decode(transactionDetails, (ConfigEpochs));
+        //console2.log("ConfigEpochs ", rawConstants.name);
+        return rawConstants;
+    }
+
+    function getConfigFarms(uint256 index) public returns (ConfigFarms memory) {
+        string memory root = vm.projectRoot();
+        string memory path = string.concat(root, "/configJSON.json");
+        string memory json = vm.readFile(path);
+        string memory indexString = string.concat(".farms", "[", Strings.toString(index), "]");
+        bytes memory transactionDetails = json.parseRaw(indexString);
+        ConfigFarms memory rawConstants = abi.decode(transactionDetails, (ConfigFarms));
         //console2.log("ConfigEpochs ", rawConstants.name);
         return rawConstants;
     }
 
     function verifyConfig(ConfigMarket memory marketConstants) public view {
-        require(marketConstants.epochBegin < marketConstants.epochEnd, "epochBegin is not < epochEnd");
-        require(marketConstants.epochEnd > block.timestamp, "epochEnd in the past");
-        require(marketConstants.strikePrice > 900000000000000000, "strikePrice is not above 0.90");
-        require(marketConstants.strikePrice < 1000000000000000000, "strikePrice is not below 1.00");
-        //TODO add more checks
+        // require(marketConstants.epochBegin < marketConstants.epochEnd, "epochBegin is not < epochEnd");
+        // require(marketConstants.epochEnd > block.timestamp, "epochEnd in the past");
+        // require(marketConstants.strikePrice > 900000000000000000, "strikePrice is not above 0.90");
+        // require(marketConstants.strikePrice < 1000000000000000000, "strikePrice is not below 1.00");
+        // //TODO add more checks
     }
 
     function verifyConfig(ConfigMarket memory marketConstants, ConfigEpochs memory epochConstants) public view {
-        require(epochConstants.epochBegin > marketConstants.epochEnd, "epochBegin is not > marketEnd");
-        require(epochConstants.epochBegin < epochConstants.epochEnd, "epochBegin is not < epochEnd");
-        require(epochConstants.epochEnd > block.timestamp, "epochEnd in the past");
-        //TODO add more checks
+        // require(epochConstants.epochBegin > marketConstants.epochEnd, "epochBegin is not > marketEnd");
+        // require(epochConstants.epochBegin < epochConstants.epochEnd, "epochBegin is not < epochEnd");
+        // require(epochConstants.epochEnd > block.timestamp, "epochEnd in the past");
+        // //TODO add more checks
 
     }
 
--- script/Script.s.sol
+++ script/Script.s.sol
@@ -7,25 +7,92 @@ import "./Helper.sol";
 //forge script ConfigScript --rpc-url $ARBITRUM_RPC_URL --private-key $PRIVATE_KEY --broadcast --skip-simulation --gas-estimate-multiplier 200 --slow -vv
 contract ConfigScript is Script, HelperConfig {
 
-    function run() public {
-        //LOAD json config and check bool deploy new markets
-        //if true deploy new markets
+    mapping (uint => bool) isIndexDeployed;
+
+    function setupY2K() public{
         ConfigAddresses memory addresses = getConfigAddresses();
         contractToAddresses(addresses);
+        setVariables();
+    }
 
+    function run() public {
+        //LOAD json config and check bool deploy new markets
+        setupY2K();
+        //if true deploy new markets
         vm.startBroadcast();
+        if(configVariables.newMarkets) {
+            //deploy new markets
+            for(uint i = 0; i < configVariables.amountOfNewMarkets;++i){
+                uint index = configVariables.marketsIds[i];
+                ConfigMarket memory markets = getConfigMarket(index);
+                ConfigEpochs memory epochs = getConfigEpochs(index);
 
-        deployMarkets(addresses);
+                //TODO verify
+                require(markets.index == epochs.index, "index of markets and epochs are not the same");
+                require(markets.index == index, "index of markets and loop are not the same");
+    
+                vaultFactory.createNewMarket(
+                    epochs.epochFee, 
+                    markets.token, 
+                    markets.strikePrice, 
+                    epochs.epochBegin, 
+                    epochs.epochEnd, 
+                    markets.oracle, 
+                    markets.name);
+
+                isIndexDeployed[index] = true;
+
+                if(configVariables.newFarms){
+                    ConfigFarms memory farms = getConfigFarms(index);
+                    require(farms.index == index, "index of farms and loop are not the same");
+                    (address _rHedge, 
+                    address _rRisk) = rewardsFactory.createStakingRewards(
+                        index, epochs.epochEnd);
+                    fundFarms(_rHedge, _rRisk, 
+                    farms.farmRewardsHEDGE, farms.farmRewardsRISK);
+                }
+            }
+        }
+        else if(configVariables.newEpochs){
+            //deploy new markets
+            for(uint i = 0; i < configVariables.amountOfNewEpochs;++i){
+                uint index = configVariables.epochsIds[i];
+                ConfigEpochs memory epochs = getConfigEpochs(index);
+
+                //TODO verify
+                require(epochs.index == index, "index of epochs and loop are not the same");
+    
+                if(!isIndexDeployed[index]){
+                    vaultFactory.deployMoreAssets(
+                        index,
+                        epochs.epochBegin, 
+                        epochs.epochEnd, 
+                        epochs.epochFee);
+
+                    isIndexDeployed[index] = true;
+
+                    if(configVariables.newFarms){
+                        ConfigFarms memory farms = getConfigFarms(index);
+                        require(farms.index == index, "index of farms and loop are not the same");
+                        (address _rHedge, 
+                        address _rRisk) = rewardsFactory.createStakingRewards(
+                            index, epochs.epochEnd);
+                        fundFarms(_rHedge, _rRisk, 
+                        farms.farmRewardsHEDGE, farms.farmRewardsRISK);
+                    }
+                }
+            }
+        }
 
         vm.stopBroadcast();
     }
 
-    function deployMarkets() public {
-       (bool newMarkets, uint256 amount, uint256[] memory  marketIds)=  getConfigMarket();
-        if(newMarkets) {
-            console2.log('newMarkets', newMarkets)
-        }
-        // vaultFactory.createNewMarket(_withdrawalFee, _token, _strikePrice, epochBegin, epochEnd, _oracle, _name);
+    function fundFarms(address _rHedge, address _rRisk, string memory _rewardsAmountHEDGE, string memory _rewardsAmountRISK) public {
+        y2k.transfer(_rHedge, stringToUint(_rewardsAmountHEDGE));
+        y2k.transfer(_rRisk, stringToUint(_rewardsAmountRISK));
+        //start rewards for farms
+        StakingRewards(_rHedge).notifyRewardAmount(stringToUint(_rewardsAmountHEDGE));
+        StakingRewards(_rRisk).notifyRewardAmount(stringToUint(_rewardsAmountRISK));
     }
 
 }
