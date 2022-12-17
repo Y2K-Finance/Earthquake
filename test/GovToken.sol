885d497cd95a4ff52d68e6bcc92a0555d0c681fd
MiguelBits <mcv.cooper@gmail.com>
2022-12-17 10:09:08 +0000

    Added Ve rewardslocker
    Author: MiguelBits

--- test/GovToken.sol
+++ test/GovToken.sol
@@ -2,12 +2,12 @@ pragma solidity 0.8.15;
 import {ERC20} from "@solmate/tokens/ERC20.sol";
 
 contract GovToken is ERC20 {
-    constructor() ERC20("Y2K", "Y2K", 18) {}
+    constructor() ERC20("Dumb", "Dumb", 18) {}
 
     function moneyPrinterGoesBrr(address _sender) public {
         _mint(_sender, 100 ether);
     }
-    
+
     function mint(address _sender) public {
         _mint(_sender, 100 ether);
     }
