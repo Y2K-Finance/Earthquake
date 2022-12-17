73868660d79c28c4ef15491edf0aacafa15047ee
MiguelBits <mcruzvas@gmail.com>
2022-12-17 13:26:25 +0000

    fix test

--- test/AssertTest.t.sol
+++ test/AssertTest.t.sol
@@ -322,8 +322,6 @@ contract AssertTest is Helper {
     function testTriggerEndEpoch() public {
         DepositDepeg();
         vm.startPrank(admin);
-        Controller test_controller = new Controller(address(vaultFactory), arbitrum_sequencer);
-        vaultFactory.setController(address(test_controller));
         vaultFactory.createNewMarket(FEE, tokenFRAX, DEPEG_AAA, beginEpoch, endEpoch, oracleFRAX, "y2kFRAX_99*");
         vm.warp(endEpoch + 1 days);
         controller.triggerEndEpoch(SINGLE_MARKET_INDEX, endEpoch);
