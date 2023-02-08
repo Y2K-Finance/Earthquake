7d71bef1e220440b6315bab1fc1b0badf45efdfa
3xHarry <caper.smock0p@icloud.com>
2023-02-08 17:41:35 +0100

    adjuist rollover logic

--- test/V2/Carousel/CarouselTest.t.sol
+++ test/V2/Carousel/CarouselTest.t.sol
@@ -8,6 +8,8 @@ import "../../../src/V2/interfaces/ICarousel.sol";
 
 
 contract CarouselTest is Helper { 
+    using stdStorage for StdStorage;
+
     // VaultV2 vaultv2;
     Carousel vault;
     address controller = address(0x54);
@@ -16,6 +18,10 @@ contract CarouselTest is Helper {
     uint256 relayerFee = 2 gwei;
     uint256 closingTimeFrame = 1000;
     uint256 lateDepositFee = 1000;
+    address USER3 = address(0x123);
+    address USER4 = address(0x345);
+    address USER5 = address(0x567);
+    address USER6 = address(0x789);
 
     function setUp() public {
 
@@ -43,8 +49,11 @@ contract CarouselTest is Helper {
         // deal(UNDERLYING, address(this), 100 ether, true);
 
         deal(UNDERLYING, USER, 1000 ether, true);
-
         deal(UNDERLYING, USER2, 1000 ether, true);
+        deal(UNDERLYING, USER3, 1000 ether, true);
+        deal(UNDERLYING, USER4, 1000 ether, true);
+        deal(UNDERLYING, USER5, 1000 ether, true);
+        deal(UNDERLYING, USER6, 1000 ether, true);
     }
 
     function testDepositInQueue() public {
@@ -66,7 +75,7 @@ contract CarouselTest is Helper {
         assertEq(vault.getDepositQueueLenght(), _queueLength);
         // test revert cases
         // should revert if epochId is 0 as this epoch is not supposed to minted ever
-        vm.expectRevert();
+        vm.expectRevert(Carousel.InvalidEpochId.selector);
         vault.mintDepositInQueue(0, _queueLength);
         // should revert if operations are not in queue length
         vm.expectRevert(Carousel.OverflowQueue.selector);
@@ -117,24 +126,6 @@ contract CarouselTest is Helper {
         assertEq(vault.balanceOfEmissions(USER2, _epochId), 10 ether - relayerFee);
     }
 
-    // function testLateDeposit() public { 
-    //     uint40 _epochBegin = uint40(block.timestamp);
-    //     uint40 _epochEnd = uint40(block.timestamp + 1 days);
-
-    //     uint256 _epochId = 1;
-    //     uint256 _emissions = 100 ether;
-
-    //     deal(emissionsToken, address(vault), 100 ether, true);
-    //     vault.setEpoch(_epochBegin, _epochEnd, _epochId);
-    //     vault.setEmissions( _epochId, _emissions);
-
-    //     vm.startPrank(USER);
-    //     vm.warp(_epochEnd - 100);
-    //     vault.deposit(0, 10 ether, USER);
-    //     vm.stopPrank();
-    // }
-
-
     function testEnListInRollover() public {
         // create two epochs
         uint40 _epochBegin = uint40(block.timestamp + 1 days);
@@ -175,14 +166,186 @@ contract CarouselTest is Helper {
         vault.setEpoch(_epochBegin, _epochEnd, _epochId);
         vault.setEmissions( _epochId, _emissions);
 
+        // let relayer rollover for user but not mit as prev epoch has not won
+        vm.startPrank(relayer);
+        vault.mintRollovers(_epochId, 1);
+        vm.stopPrank();
+
+        assertEq(vault.rolloverAccounting(_epochId), 0);
+
+        // simulate prev epoch win
+        stdstore
+            .target(address(vault))
+            .sig("claimTVL(uint256)")
+            .with_key(2)
+            .checked_write(1000 ether);
+
+        // resolve second epoch
         // let relayer rollover for user
         vm.startPrank(relayer);
         vault.mintRollovers(_epochId, 1);
         vm.stopPrank();
 
         assertEq(vault.rolloverAccounting(_epochId), 1);
-            
     }
+
+    function testDepositIntoQueueMultiple() public {
+        // test multiple deposits into queue
+        uint40 _epochBegin = uint40(block.timestamp + 1 days);
+        uint40 _epochEnd = uint40(block.timestamp + 2 days);
+        uint256 _epochId = 2;
+        uint256 _emissions = 100 ether;
+
+        deal(emissionsToken, address(vault), 100 ether, true);
+        vault.setEpoch(_epochBegin, _epochEnd, _epochId);
+        vault.setEmissions( _epochId, _emissions);
+
+        helperDepositInEpochs(_epochId,USER, true);
+        helperDepositInEpochs(_epochId,USER2, true);
+        helperDepositInEpochs(_epochId,USER3, true);
+        helperDepositInEpochs(_epochId,USER4, true);
+        helperDepositInEpochs(_epochId,USER5, true);
+        helperDepositInEpochs(_epochId,USER6, true);
+
+        assertEq(vault.getDepositQueueLenght(), 6);
+        
+        // check balance of relayer
+        uint256 balanceBefore = IERC20(UNDERLYING).balanceOf(address(this));
+
+        // mint deposit in queue
+        vault.mintDepositInQueue(_epochId, 6);
+
+        // check balance of relayer
+        uint256 balanceAfter = IERC20(UNDERLYING).balanceOf(address(this));
+
+        // check relayer fee
+        uint256 _relayerFee = (balanceAfter - balanceBefore) / 6;
+        assertEq(_relayerFee, relayerFee);
+
+        // check balances
+        assertEq(vault.balanceOf(USER, _epochId), 10 ether - relayerFee);
+        assertEq(vault.balanceOfEmissions(USER, _epochId), 10 ether - relayerFee);
+        assertEq(vault.balanceOf(USER2, _epochId), 10 ether - relayerFee);
+        assertEq(vault.balanceOfEmissions(USER2, _epochId), 10 ether - relayerFee);
+        assertEq(vault.balanceOf(USER3, _epochId), 10 ether - relayerFee);
+        assertEq(vault.balanceOfEmissions(USER3, _epochId), 10 ether - relayerFee);
+        assertEq(vault.balanceOf(USER4, _epochId), 10 ether - relayerFee);
+        assertEq(vault.balanceOfEmissions(USER4, _epochId), 10 ether - relayerFee);
+        assertEq(vault.balanceOf(USER5, _epochId), 10 ether - relayerFee);
+        assertEq(vault.balanceOfEmissions(USER5, _epochId), 10 ether - relayerFee);
+        assertEq(vault.balanceOf(USER6, _epochId), 10 ether - relayerFee);
+        assertEq(vault.balanceOfEmissions(USER6, _epochId), 10 ether - relayerFee);
+    }
+
+    function testRolloverMultiple() public {
+        // test multiple rollovers
+        // roll over users from testDepositIntoQueueMultiple test
+        testDepositIntoQueueMultiple();
+
+        // create new epoch
+        uint40 _epochBegin = uint40(block.timestamp + 3 days);
+        uint40 _epochEnd = uint40(block.timestamp + 4 days);
+        uint256 _epochId = 3;
+        uint256 _emissions = 100 ether;
+
+        deal(emissionsToken, address(vault), 100 ether, true);
+        vault.setEpoch(_epochBegin, _epochEnd, _epochId);
+        vault.setEmissions( _epochId, _emissions);
+
+        uint256 prevEpochUserBalance = 10 ether - relayerFee;
+
+        uint256 prevEpoch = 2;
+        // enlist in rollover for next epoch
+        helperRolloverFromEpoch(prevEpoch, USER,  prevEpochUserBalance);
+        helperRolloverFromEpoch(prevEpoch, USER2, prevEpochUserBalance);
+        helperRolloverFromEpoch(prevEpoch, USER3, prevEpochUserBalance);
+        helperRolloverFromEpoch(prevEpoch, USER4, prevEpochUserBalance);
+        helperRolloverFromEpoch(prevEpoch, USER5, prevEpochUserBalance);
+        helperRolloverFromEpoch(prevEpoch, USER6, prevEpochUserBalance);
+
+        // check balance of relayer
+        uint256 balanceBefore = IERC20(UNDERLYING).balanceOf(address(this));
+
+        // expect revert as prev epoch is not resolved
+        vm.expectRevert(VaultV2.EpochNotResolved.selector);
+        vault.mintRollovers(_epochId, 6);
+
+        // resolve prev epoch
+        vm.warp(block.timestamp + 2 days + 1 hours); // warp to one hour after prev epoch end
+        vm.startPrank(controller);
+        vault.resolveEpoch(prevEpoch);
+        vm.stopPrank();
+
+        // mint rollovers
+        // this should not mint any shares as prev epoch is not in profit
+        vault.mintRollovers(_epochId, 6);
+        
+        // check balance of relayer
+        uint256 balanceAfter = IERC20(UNDERLYING).balanceOf(address(this));
+        assertEq(balanceBefore, balanceAfter);
+
+        // check balances
+        assertEq(vault.balanceOf(USER, _epochId), 0);
+        assertEq(vault.balanceOfEmissions(USER, _epochId), 0);
+        assertEq(vault.balanceOf(USER2, _epochId), 0);
+        assertEq(vault.balanceOfEmissions(USER2, _epochId), 0);
+
+        // simulate prev epoch win
+        stdstore
+            .target(address(vault))
+            .sig("claimTVL(uint256)")
+            .with_key(prevEpoch)
+            .checked_write(1000 ether);
+
+        console.log("rollover queue length", vault.getRolloverQueueLenght());
+
+        // mint rollovers again
+        // this should mint shares as prev epoch is in profit
+        vault.mintRollovers(_epochId, 6);
+
+        // check balance of relayer
+        balanceAfter = IERC20(UNDERLYING).balanceOf(address(this));
+
+        // check relayer fee
+        uint256 _relayerFee = (balanceAfter - balanceBefore) / 6;
+        assertEq(_relayerFee, relayerFee);
+
+        // check balances
+        assertEq(vault.balanceOf(USER, _epochId), prevEpochUserBalance - relayerFee);
+        assertEq(vault.balanceOfEmissions(USER, _epochId), prevEpochUserBalance - relayerFee);
+        assertEq(vault.balanceOf(USER2, _epochId), prevEpochUserBalance - relayerFee);
+        assertEq(vault.balanceOfEmissions(USER2, _epochId), prevEpochUserBalance - relayerFee);
+        assertEq(vault.balanceOf(USER3, _epochId), prevEpochUserBalance - relayerFee);
+        assertEq(vault.balanceOfEmissions(USER3, _epochId), prevEpochUserBalance - relayerFee);
+        assertEq(vault.balanceOf(USER4, _epochId), prevEpochUserBalance - relayerFee);
+        assertEq(vault.balanceOfEmissions(USER4, _epochId), prevEpochUserBalance - relayerFee);
+        assertEq(vault.balanceOf(USER5, _epochId), prevEpochUserBalance - relayerFee);
+        assertEq(vault.balanceOfEmissions(USER5, _epochId), prevEpochUserBalance - relayerFee);
+        assertEq(vault.balanceOf(USER6, _epochId), prevEpochUserBalance - relayerFee);
+        assertEq(vault.balanceOfEmissions(USER6, _epochId), prevEpochUserBalance - relayerFee);
+
+        
+    }
+
+    
+
+
+    // function testLateDeposit() public { 
+    //     uint40 _epochBegin = uint40(block.timestamp);
+    //     uint40 _epochEnd = uint40(block.timestamp + 1 days);
+
+    //     uint256 _epochId = 1;
+    //     uint256 _emissions = 100 ether;
+
+    //     deal(emissionsToken, address(vault), 100 ether, true);
+    //     vault.setEpoch(_epochBegin, _epochEnd, _epochId);
+    //     vault.setEmissions( _epochId, _emissions);
+
+    //     vm.startPrank(USER);
+    //     vm.warp(_epochEnd - 100);
+    //     vault.deposit(0, 10 ether, USER);
+    //     vm.stopPrank();
+    // }
 
     function helperDepositInEpochs(uint256 _epoch, address user, bool queue) public{
         // deposit for each user in each epoch
@@ -190,7 +353,14 @@ contract CarouselTest is Helper {
         IERC20(UNDERLYING).approve(address(vault), 10 ether);
         vault.deposit(queue ? 0 : _epoch, 10 ether, user);
         vm.stopPrank();
-
     }
+
+        function helperRolloverFromEpoch(uint256 _epoch, address user, uint256 amount) public{
+        // enlist user in rollover
+        vm.startPrank(user);
+        vault.enlistInRollover(_epoch, amount, user);
+        vm.stopPrank();   
+    }
+
 
 }
