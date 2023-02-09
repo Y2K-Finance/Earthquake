6c32d510d16e453505eb6f66413870d1d2c8cffc
0xnexusflip <0xnexusflip@gmail.com>
2023-02-09 19:56:59 +0700

    add e2e depeg template

--- test/V2/Helper.sol
+++ test/V2/Helper.sol
@@ -6,13 +6,14 @@ import "./MintableToken.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Helper is Test {
+    uint256 public constant STRIKE = 1000000000000000000;
     address public constant ADMIN = address(0x1);
     address public constant WETH = address(0x888);
     address public constant TREASURY = address(0x777);
     address public constant NOTADMIN = address(0x99);
-    uint256 public constant STRIKE = 1000000000000000000;
     address public constant USER = address(0xCCA23C05a9Cf7e78830F3fd55b1e8CfCCbc5E50F);
     address public constant ARBITRUM_SEQUENCER = address(0x1c479675ad559DC151F6Ec7ed3FbF8ceE79582B6);
+    address public constant USDC_CHAINLINK = address(0x50834F3163758fcC1Df9973b6e91f0F0F0434aD3);
     address public UNDERLYING = address(0x123);
     address public TOKEN = address(new MintableToken("Token", "tkn"));
 }
--- test/V2/e2e/EndToEndV2Test.sol
+++ test/V2/e2e/EndToEndV2Test.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.17;
 
 import "../Helper.sol";
+import "../../oracles/FakeOracle.sol";
 import "../../../src/v2/VaultFactoryV2.sol";
 import "../../../src/v2/VaultV2.sol";
 import "../../../src/v2/interfaces/IVaultV2.sol";
@@ -11,14 +12,20 @@ import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 contract EndToEndV2Test is Helper {
     VaultFactoryV2 public factory;
     ControllerPeggedAssetV2 public controller;
+    FakeOracle public depegOracle;
 
     address public premium;
     address public collateral;
     address public oracle;
+    address public depegPremium;
+    address public depegCollateral;
 
     uint256 public marketId;
     uint256 public strike;
     uint256 public epochId;
+    uint256 public depegMarketId;
+    uint256 public depegStrike;
+    uint256 public depegEpochId;
 
     uint40 public begin;
     uint40 public end;
@@ -30,7 +37,6 @@ contract EndToEndV2Test is Helper {
     uint256 public constant DEALT_AMOUNT = 20 ether;
 
     function setUp() public {
-
         UNDERLYING = address(new MintableToken("UnderLyingToken", "utkn"));
 
         factory = new VaultFactoryV2(
@@ -43,7 +49,7 @@ contract EndToEndV2Test is Helper {
 
         factory.whitelistController(address(controller));
         
-        //create market - TO-DO: change oracle
+        //create end epoch market
         oracle = address(0x3);
         strike = uint256(0x2);
         string memory name = string("USD Coin");
@@ -65,13 +71,40 @@ contract EndToEndV2Test is Helper {
             )
         );
         
-        //create epoch
+        //create depeg market
+        depegOracle = new FakeOracle(USDC_CHAINLINK, 900000000000000000);
+        depegStrike = uint256(0x1);
+        (
+            depegPremium,
+            depegCollateral,
+            depegMarketId
+        ) = factory.createNewMarket(
+            VaultFactoryV2.MarketConfigurationCalldata(
+                TOKEN,
+                depegStrike,
+                address(depegOracle),
+                UNDERLYING,
+                name,
+                symbol,
+                address(controller)
+            )
+        );
+
+        //create epoch for end epoch
         begin = uint40(block.timestamp + 1 days);
         end = uint40(block.timestamp + 2 days);
         fee = uint16(0x5);
 
         epochId = factory.createEpoch(
                 marketId,
+                begin,
+                end,
+                fee
+       );
+
+       //create epoch for depeg
+        depegEpochId = factory.createEpoch(
+                depegMarketId,
                 begin,
                 end,
                 fee
@@ -113,7 +146,7 @@ contract EndToEndV2Test is Helper {
 
         //check vault balances on withdraw
         assertEq(VaultV2(premium).previewWithdraw(epochId, DEPOSIT_AMOUNT), 0);
-        assertEq(VaultV2(collateral).previewWithdraw(epochId, DEPOSIT_AMOUNT), 19.95 ether);
+        assertEq(VaultV2(collateral).previewWithdraw(epochId, DEPOSIT_AMOUNT), AMOUNT_AFTER_FEE);
 
         //withdraw from vaults
         VaultV2(premium).withdraw(epochId, DEPOSIT_AMOUNT, USER, USER);
@@ -129,4 +162,52 @@ contract EndToEndV2Test is Helper {
         vm.stopPrank();
     }
 
+    function testEndToEndDepeg() public {
+        vm.startPrank(USER);
+
+        //deal ether
+        vm.deal(USER, DEALT_AMOUNT);
+
+        //approve gov token
+        MintableToken(UNDERLYING).approve(depegPremium, DEPOSIT_AMOUNT);
+        MintableToken(UNDERLYING).approve(depegCollateral, DEPOSIT_AMOUNT);
+
+        //allowance for gov token
+        MintableToken(UNDERLYING).allowance(USER, depegPremium);
+        MintableToken(UNDERLYING).allowance(USER, depegCollateral);
+
+        //deposit in both vaults
+        VaultV2(depegPremium).deposit(depegEpochId, DEPOSIT_AMOUNT, USER);
+        VaultV2(depegCollateral).deposit(depegEpochId, DEPOSIT_AMOUNT, USER);
+
+        //check deposit balances
+        assertEq(VaultV2(depegPremium).balanceOf(USER ,depegEpochId), DEPOSIT_AMOUNT);
+        assertEq(VaultV2(depegCollateral).balanceOf(USER ,depegEpochId), DEPOSIT_AMOUNT);
+
+        //check user underlying balance
+        assertEq(USER.balance, DEALT_AMOUNT);
+
+        //warp to epoch begin
+        vm.warp(begin + 1 hours);
+        
+        //trigger depeg
+        controller.triggerDepeg(depegMarketId, depegEpochId);
+
+        //check vault balances on withdraw
+        assertEq(VaultV2(depegPremium).previewWithdraw(depegEpochId, DEPOSIT_AMOUNT), AMOUNT_AFTER_FEE);
+        assertEq(VaultV2(depegCollateral).previewWithdraw(depegEpochId, DEPOSIT_AMOUNT), 0);
+
+        //withdraw from vaults
+        VaultV2(depegPremium).withdraw(depegEpochId, DEPOSIT_AMOUNT, USER, USER);
+        VaultV2(depegCollateral).withdraw(depegEpochId, DEPOSIT_AMOUNT, USER, USER);
+
+        //check vaults balance
+        assertEq(VaultV2(depegPremium).balanceOf(USER ,depegEpochId), 0);
+        assertEq(VaultV2(depegCollateral).balanceOf(USER ,depegEpochId), 0);
+
+        //check user ERC20 balance
+        assertEq(USER.balance, DEALT_AMOUNT);
+
+        vm.stopPrank();
+    }
 }
