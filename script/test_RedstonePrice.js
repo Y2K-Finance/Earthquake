ceab31268c0190c3b769168d4592650f3c73af89
Devel Oper <developer@decelium.ai>
2023-04-20 00:41:57 +0000

    finished most of deployment forge work. Almost ready for full deployment and test

--- script/test_RedstonePrice.js
+++ script/test_RedstonePrice.js
@@ -1,16 +1,27 @@
 // testPrice.js
 
-const { ethers } = require("ethers");
-const { WrapperBuilder } = require("@redstone-finance/evm-connector");
+//const { ethers } = require("ethers");
+//const { WrapperBuilder } = require("@redstone-finance/evm-connector");
 
 async function main() {
+  
+  if (process.argv.length < 4) {
+    console.error('Error: Two positional arguments are required. (see test_RedstonePrice.js)');
+    process.exit(1);
+  }
+  const rpcUrl = process.argv[2];
+  const privateKey = process.argv[3];
+  const contractAddress = process.argv[4];
+  
   // Replace with your values
+  //const privateKey = "";
+  //const wallet = new ethers.Wallet(privateKey, provider);
+  
+  /*
   const rpcUrl = "https://goerli.infura.io/v3/37519f5fe2fb4d2cac2711a66aa06514";
-  const privateKey = "0xa881e3de2f71ddfcd7d5c189c4755b6033328d48e9895d47ea4de00603d6732c";
   const contractAddress = "0x11Cc82544253565beB74faeda687db72cd2D5d32";
 
   const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
-  const wallet = new ethers.Wallet(privateKey, provider);
     
   const abi =require("../abi/RedstonePriceProvider.sol/RedstonePriceProvider.json");
     
@@ -24,12 +35,13 @@ async function main() {
 
   const ethPriceFromContract = await wrappedContract.getLatestPrice(ethers.constants.AddressZero);
   console.log({ ethPriceFromContract });
-  
+  */
+  console.log(contractAddress);
 }
 
 main()
   .then(() => process.exit(0))
-  .catch((error) => {
+  .catch((error) => { 
     console.error(error);
     process.exit(1);
   });
